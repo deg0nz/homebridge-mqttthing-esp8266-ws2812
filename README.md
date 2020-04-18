cd790c693cd3f22828c583374b2c1c5ff9f081c5
Beh <b@kayuk.de>
2020-04-18 03:16:58 +0200

    Add first working version

---
+++ README.md
@@ -0,0 +1,57 @@
+# Homebridge MQTT-Thing ESP8266 WS2812 Controller
+
+This program lets you control WS2812 LED stripes connected to an ESP8266 via Homebridge and the Homebridge MQTT-Thing plugin. This way you can control a WS2812 LED stripe via the iOS Home.app or Siri.
+
+## Why this?
+
+I was following [Studio Pieter's HOWTO](https://www.studiopieters.nl/homebridge-homekit-2-mqtt-revised-installation/) for controlling a WS2812 LED stripes, which uses the `homekit2mqtt` Homekit bridge. But I didn't want another Homekit bridge since I already had [Homebridge](https://homebridge.io) installed and in use. So I adjusted the code from [Studio Pieter's NeoPixel-MQTT HOWTO](https://www.studiopieters.nl/homebridge-mqtt-neopixel/) to use the direct HSV values from Homebridge MQTT-Thing plugin and set the LEDs.
+
+## Preconditions
+
+* Platform.io installation
+* Homebridge installation
+* MQTT Broker installation running in your network
+* Homebridge MQTT-Thing plugin installes in Homebridge
+
+For installing Homebridge and MQTT Broker, follow the corresponding parts in [https://www.studiopieters.nl/homebridge-homekit-2-mqtt-revised-installation/](https://www.studiopieters.nl/homebridge-homekit-2-mqtt-revised-installation/). Install Homebridge MQTT-Thing via the Homebridge UI.
+
+## Configuration
+
+### ESP8266 Code
+
+* Adjust the variables at the top of `main.ino` to your needs
+* Install the NeoPixel library to the project: `platformio lib install --save "Adafruit NeoPixel"`
+* Compile the project: `platformio run`
+* Uplad the build to your ESP8266
+
+### Homebridge
+
+* You need to configure a Lightbulb - HSV item in the Homebridge MQTT-Thing plugin.
+
+Add this to the `accessories` array on your Homebridge config:
+
+``` json
+{
+    "type": "lightbulb-HSV",
+    "name": "LED Stripe",
+    "topics": {
+        "getOnline": "led-nodemcu/online",
+        "setHSV": "led-nodemcu/hsvcolor",
+        "getHSV": "led-nodemcu/hsvcolor",
+        "getOn": "led-nodemcu",
+        "setOn": "led-nodemcu"
+    },
+    "confirmationIndicateOffline": true,
+    "integerValue": false,
+    "accessory": "mqttthing"
+}
+```
+
+Adjust `led-nodemcu` above if you changed the `host` variable in `main.ino`. Also adjust the name to your needs.
+
+## Acknowledgements and sources
+
+* This is (more or less) heavily based on the codse from [https://www.studiopieters.nl/homebridge-mqtt-neopixel/](https://www.studiopieters.nl/homebridge-mqtt-neopixel/), but heavily modified.
+* Comprehension gotten from [https://www.studiopieters.nl/homebridge-homekit-2-mqtt-revised-installation/](https://www.studiopieters.nl/homebridge-homekit-2-mqtt-revised-installation/)
+* `PubSubClient` library included in this repo by [https://github.com/Imroy/pubsubclient](https://github.com/Imroy/pubsubclient)
+* `StringTokenizer` library included in this repo by [https://github.com/syalujjwal/Arduino-StringTokenizer-Library](https://github.com/syalujjwal/Arduino-StringTokenizer-Library)
---
+++ src/main.ino
@@ -0,0 +1,186 @@
+/* 
+  Project name: homebridge-mqttthing-esp8266-ws2812
+  Project URI: https://github.com/deg0nz/
+  Description: Control WS2812 LEDs with ESP8266 and Homebridge MQTT-Thing via MQTT
+  Version: 1.0.0
+  License: MIT
+*/
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <PubSubClient.h>
+#include <Adafruit_NeoPixel.h>
+#include <StringTokenizer.h>
+
+#define PIN D2 // The pin where you connect the neopixel dataline
+#define NUMPIXELS 60 // How many NeoPixels are attached
+
+const char* ssid = "MYAWESOMEWIFI";
+const char* password = "MYSUPERSECRETPASSWORD";
+const char* host = "led-nodemcu"; // the name of this device, and the mqtt base channel to listen to
+IPAddress MQTTserver(192, 168, 0, 100);
+
+// Comment, if you don't want serial output
+#define DEBUG
+
+/* Usually, there is nothing to adjust beneath this line*/
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+uint16_t hue = 0; 
+uint8_t saturation = 0;
+uint8_t value = 0; 
+uint32_t mappedHSV = 0;
+uint8_t lastKnownBrightness = 100; // Start with half of the brightness on boot
+bool isTheLightOn = false;
+
+uint16_t pixelCounter;
+uint8_t tokenCounter;
+
+String myTopic;
+String myMessage;
+
+#define BUFFER_SIZE 100
+
+WiFiClient wclient;
+PubSubClient client(wclient, MQTTserver, 1883);
+
+void fillLeds(int hue, float saturation, float value) {
+  mappedHSV = strip.gamma32(
+    strip.ColorHSV(
+      map(hue, 0, 360, 0, 65535),
+      map(saturation, 0, 100, 0, 255),
+      map(value, 0, 100, 0, 255)
+    ));
+
+  for (pixelCounter = 0; pixelCounter < strip.numPixels(); pixelCounter++) {
+    strip.setPixelColor(pixelCounter, mappedHSV);
+  }
+  strip.show();
+}
+
+void setHsvValuesFromPayload(String payload) {
+  tokenCounter = 0;
+  StringTokenizer tokenizer(payload, ",");
+  while(tokenizer.hasNext()) {
+    switch (tokenCounter) {
+        case 0:
+          hue = tokenizer.nextToken().toInt();
+          break;
+        case 1:
+          saturation = tokenizer.nextToken().toInt();
+          break;
+        case 2:
+          value = tokenizer.nextToken().toInt();
+          break;
+        default:
+          return;
+    }
+    tokenCounter++;
+  }
+}
+
+void printCurrentValues() {
+  Serial.println("");
+  Serial.println("Current State");
+  Serial.print("Hue (0-360):");
+  Serial.println(hue);
+  Serial.print("Saturation (0-100):");
+  Serial.println(saturation);
+  Serial.print("Brightness (0-100):");
+  Serial.println(value);
+  Serial.println("");
+}
+
+void mqttCallback(const MQTT::Publish& pub) {
+
+  myMessage = String(pub.payload_string());
+  myTopic = String(pub.topic());
+
+#ifdef DEBUG
+  Serial.println("=================================================");
+  // handle message arrived
+  Serial.print("Incoming message on ");
+  Serial.print(pub.topic());
+  Serial.print(" => ");
+#endif
+
+  if (myTopic == host)
+  {
+#ifdef DEBUG
+    Serial.print(pub.payload_string());
+#endif
+
+    if (pub.payload_string() == "true")
+    {
+      // Return from extra sent "on" state when adjusting brightness
+      if(isTheLightOn == true) 
+      {
+        return;
+      }
+      value = lastKnownBrightness;
+      fillLeds(hue, saturation, value);
+      isTheLightOn = true;
+    }
+    else
+    {
+      lastKnownBrightness = value;
+      value = 0;
+      fillLeds(hue, saturation, value);
+      isTheLightOn = false;
+    }
+  }
+  else if (myTopic == host + (String)"/hsvcolor") 
+  {
+#ifdef DEBUG
+    Serial.println(pub.payload_string());
+#endif
+    setHsvValuesFromPayload(pub.payload_string());
+    fillLeds(hue, saturation, value);
+  }
+
+#ifdef DEBUG
+  printCurrentValues();
+#endif
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Wifi connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // MQTT callback
+  client.set_callback(mqttCallback);
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    if (!client.connected()) {
+      if (client.connect("ESP8266: Fountain")) {
+        client.publish(host + (String)"/online", (String)"hello world, I'm " + host);
+        client.publish(host + (String)"/online", "true");
+        client.subscribe(host + (String)"/#");
+#ifdef DEBUG
+        Serial.println("MQTT client connected and subscribed");
+#endif
+      }
+    }
+
+    if (client.connected())
+      client.loop();
+  }
+}
